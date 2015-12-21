4e12506bd0dd34a25b20068a4b988a50fcac450d
Benjamin Jacobs <contact@ttocsneb.com>
2015-12-20 18:38:50 -0700

    Generated Project files

---
+++ Stranded/desktop/src/com/ttocsneb/stranded/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ttocsneb.stranded.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ttocsneb.stranded.Stranded;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Stranded(), config);
+	}
+}
